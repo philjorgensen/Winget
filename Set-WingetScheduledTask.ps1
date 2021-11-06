92a232fd85860b5102e32dfcbcc217ca6fbfa792
Philip Jorgensen <pjorgensen@lenovo.com>
2021-11-06 12:49:05 -0400

    Create Set-WingetScheduledTask.ps1

---
+++ Set-WingetScheduledTask.ps1
@@ -0,0 +1,18 @@
+$Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\AppInstallerCLI.exe")
+
+$Options = "upgrade --all -h --accept-package-agreements --accept-source-agreements"
+
+$TaskAction = New-ScheduledTaskAction -Execute $Winget -Argument $Options
+
+$TaskName = "Winget Upgrade Packages"
+
+$TaskTrigger = New-ScheduledTaskTrigger -AtLogOn
+
+$TaskUserPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -RunLevel Highest
+
+$TaskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+
+$Task = New-ScheduledTask -Action $TaskAction -Principal $TaskUserPrincipal -Trigger $TaskTrigger -Settings $TaskSettings
+
+Write-Host "Adding new Scheduled Task: $TaskName"
+Register-ScheduledTask -TaskName $TaskName -InputObject $Task -Force
